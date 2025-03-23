2ac9ac160df053ea4a24b86051f446b2a027bfed
Om Official <omstha71@gmail.com>
2025-03-23 19:06:02 +0545

    small changes

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,6 @@
 export default function Home() {
     return (
         <>
-
             <div id="home" className="home-container">
                 <div className="home-intro">
                     Where Art Meets Elegance - Professional Makeup Services
